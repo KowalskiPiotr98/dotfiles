62b87ffa77752a9219f1814065c0a9d738884c00
Piotr Kowalski <piotr@pkowalski.pw>
2022-04-12 17:56:35 +0200

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,8 @@
 alias v='nvim'
 alias e='exit'
 alias l='exa'
+alias d='sudo docker-compose'
+
+if [ -f /home/piotr/.config/fish/custom.config.fish ]
+    source /home/piotr/.config/fish/custom.config.fish
+end
