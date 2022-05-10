d6ab15f4ae272f4149fd68a13de9781b541e73e7
Piotr Kowalski <piotr@pkowalski.pw>
2022-05-10 10:36:09 +0200

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ alias v='nvim'
 alias e='exit'
 alias l='exa'
 alias d='sudo docker-compose'
-alias hibernate='sudo echo hibernating... && i3lock -f -c 000000 && sudo systemctl hibernate'
+alias hibernate='sudo systemctl hibernate'
 
 if [ -f /home/piotr/.config/fish/custom.config.fish ]
     source /home/piotr/.config/fish/custom.config.fish
