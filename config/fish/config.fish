311f16a3e1b0763ce9d5ef53df7ad19d436dcebc
Piotr Kowalski <piko98@proton.me>
2023-11-26 23:13:42 +0100

    Update dotfiles

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,7 +6,7 @@ end
 
 alias v='nvim'
 alias cat='bat'
-alias l='exa'
+alias l='eza'
 alias d='docker compose'
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
